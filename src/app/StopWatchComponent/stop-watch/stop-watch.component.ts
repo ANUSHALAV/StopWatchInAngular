b866d94480553692350c9079df45a4d92c376720
anushalav <bhattanushalav@gmail.com>
2024-01-29 16:19:20 +0530

    stop watch in angular

---
+++ src/app/StopWatchComponent/stop-watch/stop-watch.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stop-watch',
+  templateUrl: './stop-watch.component.html',
+  styleUrls: ['./stop-watch.component.css']
+})
+export class StopWatchComponent {
+
+  constructor() {
+    this.resetTime();
+  }
+
+  hrs: any = "00";
+  min: any = "00";
+  sec: any = "00";
+  timestart: any;
+  buttonDisabled = false;
+  playbutton: any;
+
+  playbtn() {
+    if (!this.buttonDisabled) {
+    
+      this.timestart = setInterval(() => {
+        this.sec++;
+        this.checkTime();
+      }, 1000);
+      
+      
+      this.buttonDisabled = true;
+    }
+  }
+
+  stopbtn() {
+    clearInterval(this.timestart);
+    this.buttonDisabled = false;
+  }
+
+  clearbtn() {
+    this.resetTime();
+    this.buttonDisabled = false;
+  }
+
+  checkTime() {
+    if (this.sec === 60) {
+      this.min++;
+      this.sec = "00";
+    }
+    if (this.min === 60) {
+      this.hrs++;
+      this.min = "00";
+    }
+  }
+
+  resetTime() {
+    this.hrs = "00";
+    this.min = "00";
+    this.sec = "00";
+  }
+}
